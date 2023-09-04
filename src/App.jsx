b270d2f0479ea8334465f61ddb26b0a8e72b9f1c
Oscar Gomez <oscargomezgomezluna@gmail.com>
2023-09-04 14:06:57 -0500

    pendiente-organizar-cambiodeimg

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import WeatherCards from "./components/WeatherCards";
+
+function App() {
+  const [coords, setCoords] = useState();
+  const [weather, setWeather] = useState();
+  const [temp, setTemp] = useState();
+  const [Image, setImage] = useState();
+
+  console.log(weather)
+
+  useEffect(() => {
+    const success = (pos) => {
+      const obj = {
+        lat: pos.coords.latitude,
+        lon: pos.coords.longitude,
+      };
+      setCoords(obj);
+    };
+    navigator.geolocation.getCurrentPosition(success);
+  }, []);
+
+  useEffect(() => {
+    if (coords) {
+      const Apikey = "db4f6ed22e243447b27536b9f42683dd";
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${Apikey}`;
+      axios
+        .get(url)
+        .then((res) => {
+          setWeather(res.data);
+          const obj = {
+            celsius: (res.data.main.temp - 273.15).toFixed(1),
+            farenheit: (((res.data.main.temp - 273.15) * 9) / 5 + 32).toFixed(
+              1
+            ),
+          };
+
+          setTemp(obj);
+        
+        
+        })
+   
+    }
+
+  
+    
+  }, [coords]);
+
+
+useEffect(() => {
+  console.log(weather?.weather[0].description)
+  if (weather?.weather[0].description !== undefined) {
+    const ImageApiKey = `39164457-e4d578415391bd203bf06bf93`;
+    const ImageUrl = `https://pixabay.com/api/?key=${ImageApiKey}&q=encodeURIComponent("${weather?.weather[0].description}")&image_type=photo`;
+    axios
+      .get(ImageUrl)
+      .then((res) => setImage(res.data))
+      .catch((err) => console.log(err));
+ 
+  }
+
+
+}, [coords])
+
+
+
+
+  console.log(Image)
+  document.body.style = `background-image: url(${Image?.hits[0].largeImageURL});`
+  
+
+  return (
+    <main className="container" >
+      <WeatherCards weather={weather} temp={temp} Image={Image} />
+      
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/WeatherCards.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+const WeatherCards = ({ weather, temp, Image }) => {
+    console.log(weather);
+    console.log(Image)
+
+  const [iscelsius, setIscelsius] = useState(true);
+
+  const handleChangeTemp = () => {
+    setIscelsius(!iscelsius);
+  };
+
+  return (
+      <article className="father">
+        
+      <div className="info-title">
+        <h1>weather app</h1>
+        <h2 className="text-center">
+          {weather?.name}, {weather?.sys.country}{" "}
+        </h2>
+      </div>
+      <div className="content">
+        <div>
+          <img
+            className="stile-img"
+            src={
+              weather &&
+              `https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`
+            }
+            alt=""
+          />
+        </div>
+        <section className="stile-list">
+          <h3>"{weather?.weather[0].description}"</h3>
+          <ul>
+            <li>
+              <span>wind speed</span>
+              <span>{weather?.wind.speed} m/s</span>
+            </li>
+            <li>
+              <span>Clouds</span>
+              <span>{weather?.clouds.all} %</span>
+            </li>
+            <li>
+              <span>Pressure</span>
+              <span>{weather?.main.pressure} hpa</span>
+            </li>
+          </ul>
+        </section>
+      </div>
+      <div className="container_btn">
+        <h2 className="container_temp">
+          {iscelsius ? `${temp?.celsius} °C` : `${temp?.farenheit} °F`}
+        </h2>
+        <button onClick={handleChangeTemp} className="btn">
+          {iscelsius ? `change to °F` : `change to °C`}
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default WeatherCards;
